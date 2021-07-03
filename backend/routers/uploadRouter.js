f8a919f8218a077bb7f9513e2a478374247c096e
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-07-03 17:05:56 +0100

    switching bucket in aws

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -34,7 +34,7 @@ aws.config.update({
 const s3 = new aws.S3();
 const storageS3 = multerS3({
     s3,
-    bucket: 'mc-gallery-ecom',
+    bucket: 'ella-crafts',
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key(req, file, cb){
